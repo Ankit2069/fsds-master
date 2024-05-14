8890f4ed1fc6423689f374d72d34d88e45a01a79
Ankit2069 <kankit2069@gmail.com>
2024-05-14 11:04:42 +0530

    change

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
-
+    return requirements
+"""
 setup(
     name='DimondPricePrediction',
     version='0.0.1',
