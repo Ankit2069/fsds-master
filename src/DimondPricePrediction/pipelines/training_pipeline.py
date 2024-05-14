fc6962c7e34a6b7c814ff92c175d149482e26a13
Ankit2069 <kankit2069@gmail.com>
2024-05-14 17:18:00 +0530

    data ingestion updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,10 +1,10 @@
-from src.DimondPricePrediction.components.data_ingestion import DataIngeestion
+from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 # import os 
 # import sys
 from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import customexception
 import pandas as pd
 
-obj=DataIngeestion()
+obj=DataIngestion()
 
 obj.initiate_data_ingestion()
